c5af7c59c8e5554ee779e25ba3b18b770dc4ed05
Brandon Lim <limws.brandon@gmail.com>
2024-04-18 10:13:25 +0800

    Add redux

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -2,7 +2,18 @@
 'use client';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import { useRef } from 'react';
+import { AppStore, makeStore } from '@/lib/store';
+import { Provider } from 'react-redux';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+  return (
+    <Provider store={storeRef.current}>
+      <ChakraProvider>{children}</ChakraProvider>
+    </Provider>
+  );
 }
