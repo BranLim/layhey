0cf27b278e16a3819943fbed2add0e4cfed38ca3
Brandon Lim <limws.brandon@gmail.com>
2024-05-22 00:26:21 +0800

    Update Add Accounting Period icon

--- app/src/components/icons/AddAccountingPeriodIcon.tsx
+++ app/src/components/icons/AddAccountingPeriodIcon.tsx
@@ -6,28 +6,28 @@ const AddAccountingPeriodIcon = createIcon({
   path: (
     <g fill='none'>
       {/* Background rectangle */}
-      <rect width='24' height='24' rx='2' ry='2' fill='#f3e5f5' />
+      <rect width='24' height='24' rx='4' ry='4' fill='#686868' />
 
       {/* Main calendar box */}
-      <rect x='3' y='4' width='18' height='18' rx='2' ry='2' fill='#ffffff' />
+      <rect x='2' y='2' width='20' height='20' rx='2' ry='2' fill='#dcdcdc' />
 
       {/* Top line separator */}
-      <rect x='3' y='8' width='18' height='1' fill='#7e57c2' />
+      <rect x='3' y='8' width='18' height='1' fill='#6a6a6a' />
 
       {/* Header blocks */}
-      <rect x='6' y='3' width='2' height='3' rx='1' ry='1' fill='#7e57c2' />
-      <rect x='16' y='3' width='2' height='3' rx='1' ry='1' fill='#7e57c2' />
+      <rect x='6' y='3' width='2' height='3' rx='1' ry='1' fill='#121212' />
+      <rect x='16' y='3' width='2' height='3' rx='1' ry='1' fill='#121212' />
 
       {/* Period blocks */}
-      <rect x='6' y='10' width='4' height='2' fill='#9575cd' />
-      <rect x='12' y='10' width='4' height='2' fill='#9575cd' />
-      <rect x='6' y='13' width='4' height='2' fill='#9575cd' />
-      <rect x='12' y='13' width='4' height='2' fill='#9575cd' />
-      <rect x='6' y='16' width='4' height='2' fill='#9575cd' />
-      <rect x='12' y='16' width='4' height='2' fill='#9575cd' />
+      <rect x='6' y='10' width='4' height='2' fill='#686868' />
+      <rect x='12' y='10' width='4' height='2' fill='#686868' />
+      <rect x='6' y='13' width='4' height='2' fill='#686868' />
+      <rect x='12' y='13' width='4' height='2' fill='#686868' />
+      <rect x='6' y='16' width='4' height='2' fill='#686868' />
+      <rect x='12' y='16' width='4' height='2' fill='#686868' />
 
       {/* Add symbol */}
-      <path d='M20 16h-2v3h-3v2h3v3h2v-3h3v-2h-3v-3z' fill='#858585' />
+      <path d='M20 16h-2v3h-3v2h3v3h2v-3h3v-2h-3v-3z' fill='#3182ce' />
     </g>
   ),
 });
