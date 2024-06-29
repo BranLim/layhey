d59f17e65096b9621f5a8e98d9df58122fa60b15
Brandon Lim <limws.brandon@gmail.com>
2024-06-29 22:46:06 +0800

    Implement drawer opening

--- app/src/components/transaction/TransactionHome.tsx
+++ app/src/components/transaction/TransactionHome.tsx
@@ -1,11 +1,16 @@
 import {
   Drawer,
+  DrawerBody,
   DrawerCloseButton,
   DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
   DrawerOverlay,
 } from '@chakra-ui/modal';
 import { useAppDispatch, useAppSelector } from '@/states/hooks';
 import { closeModal, selectIsOpenModal } from '@/states/common/modal.slice';
+import { TransactionList } from '@/components/transaction/TransactionList';
+import { Button, Flex, Spacer } from '@chakra-ui/react';
 
 export const TransactionHome = () => {
   const isOpen = useAppSelector((state) =>
@@ -17,10 +22,32 @@ export const TransactionHome = () => {
   };
 
   return (
-    <Drawer isOpen={isOpen} placement='right' onClose={handleCloseDrawer}>
+    <Drawer
+      isOpen={isOpen}
+      placement='right'
+      onClose={handleCloseDrawer}
+      size={'lg'}
+    >
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
+        <DrawerHeader>Transactions</DrawerHeader>
+        <DrawerBody>
+          <TransactionList />
+        </DrawerBody>
+        <DrawerFooter>
+          <Flex alignItems='right' pt={4} pb={2}>
+            <Spacer />
+            <Button
+              mt={4}
+              mr={2}
+              colorScheme='gray'
+              onMouseDown={() => handleCloseDrawer()}
+            >
+              Close
+            </Button>
+          </Flex>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
---
+++ app/src/components/transaction/TransactionList.tsx
@@ -0,0 +1,3 @@
+export const TransactionList = (props: any) => {
+  return <></>;
+};
