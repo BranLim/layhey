9ae8a36ef99f3ccd88cc4a4ddb3144bdcbdd66e7
Brandon Lim <limws.brandon@gmail.com>
2024-09-05 18:00:35 +0800

    Implement logic to update cashflow summaries.

--- app/src/states/features/cashflow/updateTransaction.thunk.ts
+++ app/src/states/features/cashflow/updateTransaction.thunk.ts
@@ -4,6 +4,10 @@ import { CashFlowState } from '@/states/features/cashflow/cashflow.slice';
 import { UpdateTransactionRequest } from '@/types/Transaction';
 import { updateTransactionApi } from '@/api/transactions.api';
 import { getOverallCashFlowSummary } from '@/states/features/cashflow/getOverallCashFlowSummary.thunk';
+import { getErrorMessage } from '@/utils/error.utils';
+import { isDateWithin } from '@/utils/date.utils';
+import { state } from 'sucrase/dist/types/parser/traverser/base';
+import { getCashFlows } from '@/states/features/cashflow/getCashFlow.thunk';
 
 export const updateTransaction = createAsyncThunk<
   void,
@@ -12,11 +16,13 @@ export const updateTransaction = createAsyncThunk<
 >(
   'caashflow/updateTransaction',
   async (
-    transaction: UpdateTransactionRequest,
+    updateTransactionRequest: UpdateTransactionRequest,
     { dispatch, getState }
   ): Promise<void> => {
     try {
-      const updatedTransaction = await updateTransactionApi(transaction);
+      const updatedTransaction = await updateTransactionApi(
+        updateTransactionRequest
+      );
 
       let currentState = getState();
 
@@ -34,6 +40,35 @@ export const updateTransaction = createAsyncThunk<
           reset: false,
         })
       );
-    } catch (error) {}
+
+      /*
+       * Find all the Periods (if their nodes exists in the graph)
+       * Then update the nodes' data
+       */
+      currentState = getState();
+      const currentCashFlows = currentState.cashflow.cashFlows;
+      const cashflows = Object.values(currentCashFlows).filter((statement) =>
+        isDateWithin(
+          new Date(updateTransactionRequest.transaction.date),
+          new Date(statement.accountingPeriod.startPeriod),
+          new Date(statement.accountingPeriod.endPeriod)
+        )
+      );
+      for (const cashflow of cashflows) {
+        console.log(JSON.stringify(cashflow));
+        await dispatch(
+          getCashFlows({
+            startPeriod: cashflow.accountingPeriod.startPeriod,
+            endPeriod: cashflow.accountingPeriod.endPeriod,
+            reset: false,
+            parentStatementSlotId: cashflow.parentRef ?? '',
+            parentNodeId: '',
+          })
+        );
+      }
+    } catch (error) {
+      console.log('Error updating transaction');
+      console.log(getErrorMessage(error));
+    }
   }
 );
